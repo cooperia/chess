0eac55da25add83f1cfc854b2b4fd28cf4f80ed7
Jasmina Atanassova <jasminaata@gmail.com>
2012-12-11 17:23:21 -0800

    ALL NEW!

--- chess.rb
+++ chess.rb
@@ -1,104 +1,57 @@
-#Game
 class Game
   attr_reader :pieces
   
+  def initialize(pieces)
+    @pieces = pieces
+  end
+  
+  def move_piece(args)
+    destination = args[:destination]
+    current_position = args[:current_position]
+    
+    moving_piece = pieces.find_piece_by_position(current_position)
+    dest_vacancy = pieces.check_destination_vacancy?(destination)
+    
+    if dest_vacancy
+      moving_piece.move?
+    else
+      moving_piece.capture?
+    end
+  end
+  
+  
+
+end
+
+class Pieces
+  attr_accessor :collection
+  
   def initialize
-    @pieces = []
-    for i in 1..8
-      @pieces.push(Pawn.new(:position => [i,2], :color => 'white'))
-      @pieces.push(Pawn.new(:position => [i,7], :color => 'black'))
-    end
+    @collection = [Piece.new(['A',1])]
   end
   
-  def move_piece(position, dest)
-    piece = pieces.select { |x| x.position == position}
-    piece[0].move(dest, pieces)
+  def find_piece_by_position(current_position)
+    
+  end
+  
+  def check_destination_vacancy?(destination)
+    destination
   end
 end
 
-#Piece
 class Piece
-  attr_reader :position, :color, :start
+  attr_accessor :position
   
-  def initialize(args)
-    @position = args[:position]
-    @color = args[:color]
-    @start = true
+  def initialize(position)
+    @position = position
   end
   
-  def check_position_vacancy?(dest, pieces)
-    !pieces.any? { |x| x.position == dest} 
+  def capture?(dest)
+    false
   end
   
-  def check_occupant_color(dest, pieces)
-    piece = pieces.select { |x| x.position == dest}
-    piece[0].color
+  def move?(dest)
+    true
   end
   
-  def check_move_validity?(dest)
-    if (1..8).include?(dest[0]) && (1..8).include?(dest[1]) 
-      true
-    else
-      false
-    end
-  end
-end
-
-#Pawn
-class Pawn < Piece
-  
-  def check_double_move?(pieces)
-    if check_position_vacancy?([position[0], 3], pieces) && check_position_vacancy?([position[0], 4], pieces)
-      true
-    else
-      false
-    end
-  end  
-  
-  def check_move_legality?(dest, pieces)
-    if start && dest[1] == 4
-      check_double_move?(pieces) 
-    elsif dest[1] == position[1]+1
-      check_position_vacancy?(dest, pieces)
-    else
-      false
-    end
-  end
-  
-  def check_capture_legality?(dest)
-    if dest == [position[0]+1, position[1]+1] || dest == [position[0]+1, position[1]-1]
-      true
-    else
-      false
-    end
-  end
-  
-  def check_capture?(dest, pieces)
-    if check_capture_legality?(dest) && !check_position_vacancy?(dest, pieces) && check_occupant_color(dest, pieces) != color
-      true
-    else
-      false
-    end
-  end
-  
-  def move(dest, pieces)
-    if check_move_validity?(dest)
-      if dest[0] != position[0]
-        if check_capture?(dest, pieces)
-          pieces.delete_if { |x| x.position == dest }
-          start = false
-          position = dest
-        else
-          "Illegal Capture"
-        end
-      elsif check_move_legality?(dest, pieces)
-        start = false
-        position = dest
-      else
-        "Illegal Move"
-      end
-    else
-      "Invalid Destination"
-    end
-  end
 end
--- chess_spec.rb
+++ chess_spec.rb
@@ -1,110 +1,81 @@
-require '~/Documents/Ruby/chess/chess.rb'
-
-describe Piece do
-  describe '#new' do
-    it "Accepts 2 params: start position and color. Returns Piece object" do
-      piece = Piece.new(:position => [1,2], :color => 'white')
-      piece.position.should eq([1,2])
-      piece.color.should eq('white')
-      piece.start.should eq(true)
-    end
-  end
-  
-  describe '#check_position_vacancy' do
-    it "Accepts a pieces array and a destination and determines if dest is occupied" do
-      pieces = []
-      piece = Piece.new(:position => [1,2], :color => 'white')
-      pieces.push(Pawn.new(:position => [1,4], :color => 'white'))
-      piece.check_position_vacancy?([1,4], pieces).should eq(false)
-    end
-  end
-  
-  describe '#check_occupant_color' do
-    it "Accepts a pieces array and a destination and the color of the occupying piece" do
-      pieces = []
-      piece = Piece.new(:position => [1,2], :color => 'white')
-      pieces.push(Pawn.new(:position => [1,4], :color => 'black'))
-      piece.check_occupant_color([1,4], pieces).should eq("black")
-    end
-  end
-  
-  describe '#check_move_validity' do
-    it "Accepts a pieces array and determines if dest is on the board" do
-      piece = Piece.new(:position => [1,1], :color => 'white')
-      piece.check_move_validity?([0,4]).should eq(false)
-      piece.check_move_validity?([1,4]).should eq(true)
-    end
-  end
-  
-end
-
-describe Pawn do
-  describe '#check_double_move' do
-    it "Accepts the pieces array and determines if a move double move is legal" do
-      pawn = Pawn.new(:position => [1,2], :color => 'white')
-      pieces = [Pawn.new(:position => [1,3], :color => 'white')]
-      pawn.check_double_move?(pieces).should eq(false)
-    end
-  end
-  
-  describe '#check_move_legality' do
-    it "Accepts destination and pieces array to determin if a move is legal" do
-      pawn = Pawn.new(:position => [1,2], :color => 'white')
-      pieces = [Pawn.new(:position => [1,4], :color => 'white')]
-      pawn.check_move_legality?([1,3], pieces).should eq(true)
-      pawn.check_move_legality?([1,4], pieces).should eq(false)
-      pawn.check_move_legality?([1,5], pieces).should eq(false)
-    end
-  end
-  
-  describe '#check_capture_legality' do
-    it "Accepts destination and determins if destination is a legal capture location" do
-      pawn = Pawn.new(:position => [1,2], :color => 'white')
-      pawn.check_capture_legality?([1,3]).should eq(false)
-      pawn.check_capture_legality?([2,3]).should eq(true)
-    end
-  end
-  
-  describe '#check_capture' do
-    it "Accepts destination and pieces array to determin if a capture possible" do
-      pawn = Pawn.new(:position => [1,2], :color => 'white')
-      pieces = [Pawn.new(:position => [2,3], :color => 'black')]
-      pawn.check_capture?([1,3], pieces).should eq(false)
-      pawn.check_capture?([2,3], pieces).should eq(true)
-    end
-  end
-  
-  describe '#move' do
-    it "Accepts destination and pieces array, performs appropriate action and catches illegality." do
-      pawn = Pawn.new(:position => [1,2], :color => 'white')
-      pawn2 = Pawn.new(:position => [3,2], :color => 'white')
-      pieces = [Pawn.new(:position => [1,4], :color => 'white'), Pawn.new(:position => [2,3], :color => 'black')]
-      pawn.move([1,4], pieces).should eq("Illegal Move")
-      pawn.move([1,5], pieces).should eq("Illegal Move")
-      pawn.move([2,4], pieces).should eq("Illegal Capture")
-      pawn.move([2,3], pieces).should eq([2,3])
-      pawn2.move([0,4], pieces).should eq ("Invalid Destination")
-      pawn2.move([3,3], pieces).should eq([3,3])
-    end
-  end
-end
+require 'rubygems'
+require 'rspec'
+require '/Users/jasmina/Documents/Ruby/chess2/chess.rb'
 
 describe Game do
+  
+  let(:pieces) { Pieces.new }
+  let(:game) { Game.new(pieces) }
+  
+  it 'has pieces' do 
+    game.pieces
+  end
+  
   describe '#new' do
-    it "Fills a pieces array with white and black pieces" do
-      game = Game.new
-      for i in 0..7
-        game.pieces[i*2].color.should eq('white')
-        game.pieces[i*2+1].color.should eq('black')
+    it "Starts a game" do
+      game.pieces == pieces
       end
-    end
   end
   
   describe '#move_piece' do
-    it "Accepts position of piece in question and destination, performs move." do
-      game = Game.new
-      game.move_piece([1,2], [1,3]).should eq([1,3])
-      game.move_piece([1,2], [2,3]).should eq("Illegal Capture")
+    it "accepts a hash containing position and destination returns true if valid move" do
+      # current_position = game.move_piece({:current_position => ['A',1], :destination => ['A', 2]})
+      # current_position[:current_position].should eq(['A',1])
+      # current_position[:destination].should eq(['A',2])
+      # piece = mock('Piece').stub(:move?).and_return true
+      #       
+      #       pieces = Pieces.new([piece])
+      #       Game.new(pieces)
+      #       
+    end
+  end
+  
+  
+end
+
+describe Pieces do
+  let(:pieces) { Pieces.new }
+  let(:current_piece) {pieces.collection[0]}
+  let(:dest) {['A',2]}
+  
+  describe '#new' do
+    it "creates a collection array" do
+      pieces.collection[0].should == current_piece
+      
+    end
+  end
+  
+  describe '#find_piece_by_position' do
+    it 'finds a piece from current position' do
+      pieces.find_piece_by_position(['A',1]).should == current_piece
+    end
+  end
+  
+  describe '#check_destination_vacancy?' do
+    it 'checks the vacancy of a destination' do
+      pieces.check_destination_vacancy?(dest).should == ['A',2]
+    end
+  end
+end
+
+describe Piece do
+  let(:piece) {Piece.new(['A',2])}
+  
+  describe '#new' do
+    it "creates a piece and takes position" do
+      piece.position
+    end
+  end
+  
+  describe '#capture?' do
+    it "checks if a destination is a legal capture" do
+      piece.capture?('destination').should == false
+    end
+  end
+  
+  describe '#move?' do
+    it "checks if a destination is a legal move" do
+      piece.move?('destination').should == true
     end
   end
 end
