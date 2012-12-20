42e501c139c6aa46c98f8037d08ef1a39f9508a5
Jasmina Atanassova <jasminaata@gmail.com>
2012-12-20 12:38:22 -0800

    Remove initializer from RookPathGenerator. Refactor specs

--- lib/rook_path_generator.rb
+++ lib/rook_path_generator.rb
@@ -1,30 +1,23 @@
 class RookPathGenerator
-  attr_accessor :move, :path
-
-  def initialize(move)
-     @move = move
-     @path = Path.new
-  end
-
   def self.generate(move)
-    generator = RookPathGenerator.new(move)
-    a_ranged = generator.arranger
+    a_ranged = self.arranger(move)
+    path = Path.new
     (a_ranged.first.next...a_ranged.last).each do |entry|
-      generator.path.add(Coordinate.new(entry))
+      path.add(Coordinate.new(entry))
     end
-    generator.cleaner(position) if generator.horizontal?
-    generator.path
+    self.cleaner(path, move.origin) if self.horizontal?(path)
+    path
   end
 
-  def horizontal?
+  def self.horizontal?(path)
     path.size > 6
   end
 
-  def cleaner
-    self.path = path.clean(move.origin)
+  def self.cleaner(path, origin)
+    path.clean(origin)
   end
 
-  def arranger
+  def self.arranger(move)
     [move.origin.stringify, move.destination.stringify].sort
   end
 end
--- spec/rook_path_generator_spec.rb
+++ spec/rook_path_generator_spec.rb
@@ -1,11 +1,13 @@
 require 'spec_helper'
 
 describe RookPathGenerator do
+  let(:pieces) { Pieces.new }
+  let(:vertical_move) { Move.new('A1', 'A4', pieces) }
+  let(:path_double) { double("Path Object") }
 
   describe '#generate' do
     it 'generates a path that must be open to move a piece' do
-      move = Move.new('A1', 'A4', Pieces.new)
-      path = RookPathGenerator.generate(move)
+      path = RookPathGenerator.generate(vertical_move)
       path.included?(Coordinate.new('A2')).should == true
       path.included?(Coordinate.new('A3')).should == true
       path.included?(Coordinate.new('A4')).should == false
@@ -13,71 +15,39 @@ describe RookPathGenerator do
   end
 
   describe '#horizontal?' do
-    it 'returns true if the path\'s size is greater than 6 (horizontal move)' do
-      generator = RookPathGenerator.new(Move.new('A1', 'B1', Pieces.new))
-      (1..8).each do |i|
-        generator.path.add(Coordinate.new('A'+i.to_s))
+    context "when the paths size is greater than 6" do
+      it "returns true" do
+        path_double.stub(:size).and_return(7)
+        RookPathGenerator.horizontal?(path_double).should == true
       end
-      generator.horizontal?.should == true
+    end
 
+    context "when the paths size is less than 6" do
+      it "returns false" do
+        path_double.stub(:size).and_return(5)
+        RookPathGenerator.horizontal?(path_double).should == false
+      end
     end
   end
 
   describe '#cleaner' do
-    it 'asks the path to clean itself of extraneous positions for horizontal moves' do
-        generator = RookPathGenerator.new(Move.new('A1', 'B1', Pieces.new))
-        (1..8).each do |i|
-          generator.path.add(Coordinate.new('A'+i.to_s))
-        end
-        generator.cleaner
-        generator.path.size.should == 1
+    it 'it calls clean on path' do
+      path_double.should_receive(:clean).with('something')
+      RookPathGenerator.cleaner(path_double, 'something')
     end
   end
 
   describe '#arranger' do
-    it 'return an array of position and destination, smallest to largest as strings' do
-      generator = RookPathGenerator.new(Move.new('C1', 'A1', Pieces.new))
-      generator.arranger.should == ['A1', 'C1']
+    it 'return an array of position and destination' do
+      vertical_move.stub(:origin).and_return(Coordinate.new("A1"))
+      vertical_move.stub(:destination).and_return(Coordinate.new("A5"))
+      RookPathGenerator.arranger(vertical_move).should == ["A1", "A5"]
+    end
+
+    it "is sorted" do
+      vertical_move.stub(:origin).and_return(Coordinate.new("A5"))
+      vertical_move.stub(:destination).and_return(Coordinate.new("A1"))
+      RookPathGenerator.arranger(vertical_move).should == ["A1", "A5"]
     end
   end
 end
-
-
-  #describe 'valid_move?' do
-  #  it 'returns true if a destination is valid within the set of rules' do
-  #    valid_moves = path_generator.generate_moves(Coordinate.new('A1'))
-  #    path_generator.valid_move?(valid_moves, Coordinate.new('A3')).should == true
-  #  end
-  #
-  #  it 'returns false if it isnt' do
-  #    valid_moves = path_generator.generate_moves(Coordinate.new('A1'))
-  #    path_generator.valid_move?(valid_moves, Coordinate.new('B3')).should == false
-  #  end
-  #end
-  #
-  #describe '#generate_moves' do
-  #  it 'takes a position and generates an array of possible moves' do
-  #    moves = path_generator.generate_moves(Coordinate.new('A1'))
-  #    moves.each do |entry|
-  #      entry.equal?(Coordinate.new('A1')).should == false
-  #    end
-  #    moves.empty?.should == false
-  #  end
-  #end
-  #
-  #describe '#generate_moves' do
-  #  it 'takes a position and generates an array of possible moves' do
-  #
-  #    allowed_moves = [Coordinate.new('A2'), Coordinate.new('A3'), Coordinate.new('A4'), Coordinate.new('A5'), Coordinate.new('A6'), Coordinate.new('A7'), Coordinate.new('A8'), Coordinate.new('B1'), Coordinate.new('C1'), Coordinate.new('D1'), Coordinate.new('E1'), Coordinate.new('F1'), Coordinate.new('G1'), Coordinate.new('H1')]
-  #    path_generator.generate_moves(Coordinate.new('A1')).each do |generated_move|
-  #      allowed_moves.find {|allowed_move| allowed_move.equal?(generated_move)}.is_a?(Coordinate).should == true
-  #    end
-  #    path_generator.generate_moves(Coordinate.new('A1')).length.should == allowed_moves.length
-  #  end
-  #end
-  #
-  #describe '#path_array' do
-  #  it 'returns an array of squares that must be open for a given move' do
-  #    path_generator.path_array(['A1','C1'], Coordinate.new('A1')).first.equal?(Coordinate.new('B1')).should == true
-  #  end
-  #end
