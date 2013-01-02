544d76d124a39b8b4d17bb56e7546288bc989986
Jasmina Atanassova <jasminaata@gmail.com>
2013-01-02 10:42:26 -0800

    [#41496891] Small refactor to capture_at in board.

--- lib/board.rb
+++ lib/board.rb
@@ -11,8 +11,12 @@ class Board
 
   def capture_at(destination, piece)
     capture = find_at(destination)
-    capture_errors(piece, capture) if capture.is_a?(Piece)
-    remove_piece(destination) if capture.is_a?(Piece)
+    complete_capture(piece, capture, destination) if capture.is_a?(Piece)
+  end
+
+  def complete_capture(piece, capture, destination)
+    capture_errors(piece, capture)
+    remove_piece(destination)
   end
 
   def capture_errors(piece, capture)
@@ -31,69 +35,4 @@ class Board
   def place_error_catcher(position)
     raise('Position occupied') if find_at(position)
   end
-
-
-
-
-
-
-
-
-
-
-
-
-  #
-  #
-  #
-  #
-  #
-  #def get_moving_piece(move)
-  #  raise('No piece at position') unless find_at(move.position)
-  #  find_at(move.position)
-  #end
-  #
-  #def complete_move(piece, path, move)
-  #  obstructed?(path)
-  #  move_type = check_move_type(move)
-  #  send("complete_#{move_type}_move", piece, move)
-  #end
-  #
-  #def check_move_type(move)
-  #  vacancy?(move.destination) ? 'regular' : 'capture'
-  #end
-  #
-  #def complete_regular_move(piece, move)
-  #  update_position(piece, move.destination)
-  #  true
-  #end
-  #
-  #def complete_capture_move(piece, move)
-  #  capture_error_catcher(move)
-  #  destroy_piece(move.destination)
-  #  complete_regular_move(piece, move)
-  #end
-  #
-  #
-  #
-  #def verify_move(move)
-  #  vacancy?(move.destination) ? nil : capture_error_catcher(move)
-  #end
-  #
-  #
-  #
-  #def update_position(piece, move_destination)
-  #  piece.set_position(move_destination)
-  #end
-  #
-  #
-  #
-  ##TODO:Possibly move all errors into an error class and pass self and state
-  #
-  #
-  #
-  #
-  #def vacancy?(destination)
-  #  !collection.any? { |piece| piece.position.equal?(destination) }
-  #end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -16,33 +16,44 @@ describe Board do
   end
 
   describe '#capture_at' do
-    let(:piece) {board.find_at(Coordinate.new('A1'))}
-    let(:target_piece) {board.find_at(Coordinate.new('B1'))}
-    context 'if there is a piece at destination' do
-      before do
-        board.place('rook', 'white', 'B1')
-      end
-      it 'should call capture_errors' do
-        board.should_receive(:capture_errors).with(piece, target_piece).and_return(true)
-        board.capture_at(move.destination, piece)
-      end
+    let(:piece) {Piece.new('rook', 'black', 'A1')}
+    let(:destination) {Coordinate.new('B1')}
+    let(:target_piece) {Piece.new('rook', 'white', 'B1')}
+    it 'should find a capturable piece and call complete_capture' do
+      board.should_receive(:find_at).with(destination)
+      board.capture_at(destination, piece)
+    end
 
-      it 'should call remove_piece' do
-        board.should_receive(:remove_piece).with(move.destination)
-        board.capture_at(move.destination, piece)
+    context 'capture is a Piece' do
+      it 'should call #complete_capture' do
+        board.stub(:find_at) {target_piece}
+        board.should_receive(:complete_capture).with(piece, target_piece, destination)
+        board.capture_at(destination, piece)
       end
     end
 
-    context 'if there is no piece at destination' do
-      it 'should not call capture_errors' do
-        board.should_not_receive(:capture_errors)
-        board.capture_at(move.destination, piece)
+    context 'capture is not a Piece' do
+      it "should not call #complete_capture" do
+        board.stub(:find_at) {nil}
+        board.should_not_receive(:complete_capture)
+        board.capture_at(destination, piece)
       end
+    end
+  end
 
-      it 'should not receive remove_piece' do
-        board.should_not_receive(:remove_piece)
-        board.capture_at(move.destination, piece)
-      end
+  describe '#complete_capture' do
+    let(:piece) {Piece.new('rook', 'black', 'A1')}
+    let(:destination) {Coordinate.new('B1')}
+    let(:target_piece) {Piece.new('rook', 'white', 'B1')}
+
+    it 'should call capture_errors' do
+      board.should_receive(:capture_errors).with(piece, target_piece).and_return(true)
+      board.complete_capture(piece, target_piece, destination)
+    end
+
+    it 'should call remove_piece' do
+      board.should_receive(:remove_piece).with(destination)
+      board.complete_capture(piece, target_piece, destination)
     end
   end
 
@@ -81,128 +92,4 @@ describe Board do
     end
 
   end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  #
-  #
-  #describe '#get_moving_piece' do
-  #  it 'returns a piece at the specified position' do
-  #    move2 = Move.new('A2', 'B1')
-  #    piece2 = pieces.place('rook', 'black', 'A2')
-  #    pieces.get_moving_piece(move2).should == piece2
-  #  end
-  #
-  #  it 'raises an exception if there is no piece at position' do
-  #    bad_move = Move.new('B1', 'C1')
-  #    expect { pieces.get_moving_piece(bad_move) }.to raise_error(RuntimeError, 'No piece at position')
-  #  end
-  #end
-  #
-  #describe '#complete move' do
-  #  it 'should verify complete regular move methods' do
-  #    piece = Piece.new('rook','black','A1')
-  #    path = [Coordinate.new('A2'), Coordinate.new('A3')]
-  #    pieces.should_receive(:path_check?).with(path)
-  #    pieces.should_receive(:check_move_type).with(move).and_return('regular')
-  #    pieces.should_receive(:complete_regular_move).with(piece, move)
-  #    pieces.complete_move(piece, path, move)
-  #  end
-  #
-  #  it 'should verify complete capture move methods' do
-  #    piece = Piece.new('rook','black','A1')
-  #    path = [Coordinate.new('A2'), Coordinate.new('A3')]
-  #    pieces.place('rook', 'white', 'B1')
-  #    pieces.should_receive(:path_check?).with(path)
-  #    pieces.should_receive(:check_move_type).with(move).and_return('capture')
-  #    pieces.should_receive(:complete_capture_move).with(piece, move)
-  #    pieces.complete_move(piece, path, move)
-  #  end
-  #end
-  #
-  #describe '#check_move_type' do
-  #  it 'should return regular if destination is vacant' do
-  #    pieces.check_move_type(move).should == 'regular'
-  #  end
-  #
-  #  it 'should return capture if destination is occupied' do
-  #    pieces.place('rook', 'black', 'B1')
-  #    pieces.check_move_type(move).should == 'capture'
-  #  end
-  #end
-  #
-  #describe '#complete_regular_move' do
-  #  it 'updates the position of a piece and returns true' do
-  #    piece = Piece.new('rook','black','A1')
-  #    pieces.complete_regular_move(piece, move).should == true
-  #    piece.position.equal?(Coordinate.new('B1')).should == true
-  #  end
-  #end
-  #
-  #describe '#compete_capture_move' do
-  #  it 'performs a capture move' do
-  #    piece = Piece.new('rook','black','A1')
-  #    pieces.should_receive(:capture_error_catcher).with(move)
-  #    pieces.should_receive(:destroy_piece).with(move.destination)
-  #    pieces.should_receive(:complete_regular_move).with(piece, move)
-  #    pieces.complete_capture_move(piece, move)
-  #  end
-  #end
-  #
-  #describe '#capture_error_catcher' do
-  #  it 'catches if the destination is occupied by the same color' do
-  #    pieces.place('rook', 'black', 'B1')
-  #    expect { pieces.capture_error_catcher(move) }.to raise_error(RuntimeError, 'Can\'t capture your own pieces!')
-  #  end
-  #end
-  #
-  #
-  #
-  #describe '#destroy_piece' do
-  #  it 'should destroy piece at destination' do
-  #    pieces.place('rook', 'black', 'H8')
-  #    pieces.destroy_piece(Coordinate.new('H8'))
-  #    pieces.find_at(Coordinate.new('H8')).should == nil
-  #  end
-  #end
-  #
-  #describe '#update_position' do
-  #  it 'should update a piece\'s destination' do
-  #    piece = pieces.collection.first
-  #    pieces.update_position(piece, Coordinate.new('A4'))
-  #    piece.position.equal?(Coordinate.new('A4')).should == true
-  #  end
-  #end
-  #
-  #
-  #
-  ##describe '#path_check?' do
-  ##  it 'returns false if a path is obstructed' do
-  ##    expect { pieces.path_check?([Coordinate.new('A1'), Coordinate.new('A2')])}.to raise_error(RuntimeError, 'Path obstructed')
-  ##  end
-  ##
-  ##  it 'returns true if path is unobstructed' do
-  ##    pieces.path_check?([Coordinate.new('A2'), Coordinate.new('A3')]).should == true
-  ##  end
-  ##end
-  #
-  #
-  #
-  #describe '#vacancy?' do
-  #  it 'checks the vacancy of a destination' do
-  #    pieces.vacancy?(Coordinate.new('A2')).should == true
-  #    pieces.vacancy?(Coordinate.new('A1')).should == false
-  #  end
-  #end
 end
